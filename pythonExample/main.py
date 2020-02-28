bc42ad10a465c0eb96dbb33fefd5146e83e541db
Ignacio Gomez <ignacio.gomez@solarisbank.de>
2020-02-28 22:43:07 +0100

    extract btc price

--- pythonExample/main.py
+++ pythonExample/main.py
@@ -1,6 +1,22 @@
 import datetime
 import time
-print("Hello")
+import http.client
+import mimetypes
+import json
+
+
+def getBTCPrice():
+    conn = http.client.HTTPSConnection("api.coindesk.com")
+    payload = ''
+    headers = {}
+    conn.request("GET", "/v1/bpi/currentprice.json", payload, headers)
+    res = conn.getresponse()
+    data = res.read().decode("utf-8")
+    loaded_json = json.loads(data)
+    return loaded_json["bpi"]["USD"]["rate"]
+
+
 while True:
-    print(datetime.datetime.now())
+    print(getBTCPrice())
+    # print(datetime.datetime.now())
     time.sleep(1)
