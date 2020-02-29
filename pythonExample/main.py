01cf58b8fc9cae88b0a08b13d70f1e24a037dc28
Ignacio Gomez <ignacio.gomez@solarisbank.de>
2020-02-29 01:18:13 +0100

    tracing

--- pythonExample/main.py
+++ pythonExample/main.py
@@ -5,6 +5,20 @@ import time
 import http.client
 import mimetypes
 import json
+from opencensus.ext.stackdriver import trace_exporter as stackdriver_exporter
+import opencensus.trace.tracer
+
+
+def initialize_tracer(project_id):
+    exporter = stackdriver_exporter.StackdriverExporter(
+        project_id=project_id
+    )
+    tracer = opencensus.trace.tracer.Tracer(
+        exporter=exporter,
+        sampler=opencensus.trace.tracer.samplers.AlwaysOnSampler()
+    )
+
+    return tracer
 
 
 def getBTCPrice():
@@ -21,11 +35,17 @@ def getBTCPrice():
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/price/btc/usd')
 def getPrice():
+    tracer = app.config['TRACER']
+    tracer.start_span(name='price/btc/usd')
     price = getBTCPrice()
+    tracer.end_span()
     return price
 
 
 if __name__ == "__main__":
+    tracer = initialize_tracer(os.environ.get("GOOGLE_CLOUD_PROJECT"))
+    app.config['TRACER'] = tracer
+
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
