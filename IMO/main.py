42d529cc2fe5694a84f9bf698c58a45c29e1e4e7
Ignacio Gomez <ignacio.gomez.arboleda@gmail.com>
2020-03-01 15:06:32 +0100

    add loading environment variables from file

--- IMO/main.py
+++ IMO/main.py
@@ -2,6 +2,10 @@ import mimetypes
 import http.client
 import time
 import os
+import threading
+from flask import Flask
+from dotenv import load_dotenv
+from pathlib import Path  # python3 only
 
 
 def checkIfPropertyExists(propertyID):
@@ -34,7 +38,38 @@ def scanProperty(propertyID):
 
 counter = 0
 biggestIdentifier = 114988500
+started = False
+app = Flask(__name__)
 
-while True:
-    scanProperty(biggestIdentifier+counter)
-    counter = counter + 1
+
+def scanProperties():
+    while True:
+        global counter
+        scanProperty(biggestIdentifier + counter)
+        counter = counter + 1
+        time.sleep(1)
+
+
+@app.route('/')
+def getPrice():
+    global started
+    if started:
+        return "OK"
+    started = True
+    x = threading.Thread(target=scanProperties)
+    x.start()
+    return 201
+
+
+def setupEnvs():
+    env_path = Path('.') / '.env'
+    load_dotenv(dotenv_path=env_path, verbose=True)
+
+
+if __name__ == "__main__":
+    setupEnvs()
+    started = False
+    debug = os.getenv("_DEBUG", False)
+    app.run(debug=debug,
+            host='0.0.0.0',
+            port=int(os.environ.get('PORT', 8080)))
