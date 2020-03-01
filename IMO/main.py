5c44dcb1421127f093c382cbd501823c67843ec4
Ignacio Gomez <ignacio.gomez.arboleda@gmail.com>
2020-03-01 16:01:05 +0100

    adjust logging

--- IMO/main.py
+++ IMO/main.py
@@ -6,7 +6,7 @@ import threading
 from flask import Flask
 from dotenv import load_dotenv
 from pathlib import Path  # python3 only
-
+import logging
 
 def checkIfPropertyExists(propertyID):
     base_url = os.environ.get("_BASE_URL")
@@ -15,12 +15,9 @@ def checkIfPropertyExists(propertyID):
     headers = {}
     conn.request("HEAD", "/expose/"+str(propertyID), payload, headers)
     res = conn.getresponse()
-    cases = {
-        200: True,
-    }
 
-    if res.code in cases:  # exists
-        return cases[res.code]
+    if res.code == 200:
+        return True
 
     return False
 
@@ -31,8 +28,12 @@ def postProperty(id):
 
 
 def scanProperty(propertyID):
+    logging.debug("Looking for property:")
+    logging.debug(propertyID)
     exists = checkIfPropertyExists(propertyID)
     if exists:
+        logging.debug("Property exists:")
+        logging.debug(propertyID)
         postProperty(propertyID)
 
 
@@ -43,6 +44,7 @@ app = Flask(__name__)
 
 
 def scanProperties():
+    logging.info("Started scanning properties")
     while True:
         global counter
         scanProperty(biggestIdentifier + counter)
@@ -58,7 +60,7 @@ def getPrice():
     started = True
     x = threading.Thread(target=scanProperties)
     x.start()
-    return 201
+    return "Started"
 
 
 def setupEnvs():
@@ -69,6 +71,10 @@ def setupEnvs():
 if __name__ == "__main__":
     setupEnvs()
     started = False
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format,
+                        level=logging.DEBUG,
+                        datefmt="%H:%M:%S")
     debug = os.getenv("_DEBUG", False)
     app.run(debug=debug,
             host='0.0.0.0',
